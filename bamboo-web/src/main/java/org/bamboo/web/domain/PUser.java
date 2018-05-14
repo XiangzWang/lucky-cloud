8e830511a694ed57d1a75091515ba591684d6216
XiangzWang <294059349@qq.com>
2018-05-15 02:01:10 +0800

    增加druid监控及日志

--- bamboo-web/src/main/java/org/bamboo/web/domain/PUser.java
+++ bamboo-web/src/main/java/org/bamboo/web/domain/PUser.java
@@ -36,4 +36,41 @@ public class PUser {
 		return "PUser [id=" + id + ", name=" + name + ", sex=" + sex + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((sex == null) ? 0 : sex.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PUser other = (PUser) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (sex == null) {
+			if (other.sex != null)
+				return false;
+		} else if (!sex.equals(other.sex))
+			return false;
+		return true;
+	}
+
 }
